4ffaf08cc7ee04ac809bb6e26d9e2aac7afaea37
UGcode101 <142420124+UGcode101@users.noreply.github.com>
2024-05-13 20:28:54 -0500

    complete

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -4,6 +4,7 @@ public class User {
     private String username;
     private String email;
     private String password;
+    private String verifyPassword;
 
     public User() {
 
@@ -38,6 +39,23 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
+    }
+
+    private void checkPassword() {
+        if (password != null && verifyPassword != null) {
+            if (!password.equals(verifyPassword)) {
+                verifyPassword = null;
+            }
+        }
     }
 }
-
